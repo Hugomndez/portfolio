ba9899529819cfd8152a91c42a9b299b745e42be
Hugomndez <hello@hugomendez.dev>
2022-12-07 23:48:13 -0500

    style: implement @next/font Space Grotesk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
+import { Space_Grotesk } from '@next/font/google';
 import type { AppProps } from 'next/app';
 import 'styles/globals.css';
 
+const font = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['500', '700'],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={font.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
