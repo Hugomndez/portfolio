c5d29da4e5f66f4d7c54a1b8df156708422bc2d1
Hugo Méndez <hello@hugomendez.dev>
2023-05-22 01:12:46 -0500

    style: remove white-space

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -1,11 +1,8 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     readonly VERCEL_ENV: string;
-
     readonly NEXTAUTH_URL: string;
-
     readonly SENDGRID_API_KEY: string;
-
     readonly NEXT_PUBLIC_RECAPTCHA_SITE_KEY: string;
     readonly RECAPTCHA_SECRET_KEY: string;
   }
