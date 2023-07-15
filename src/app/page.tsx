b8b25878be2adf15736107ddedb00ffa4bd27123
Hugo Méndez <hello@hugomendez.dev>
2023-07-15 12:20:03 -0500

    feat: add umami analytics script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import { Bar, Contact, Intro, Projects, Skills } from 'components';
+import Script from 'next/script';
 
 export default function Page() {
   return (
     <>
+      <Script
+        id='umami-analytics'
+        src='https://analytics.umami.is/script.js'
+        data-website-id='8110a8a7-c042-4ada-8306-ed6b7327a5b7'
+      />
       <header>
         <Intro />
       </header>
