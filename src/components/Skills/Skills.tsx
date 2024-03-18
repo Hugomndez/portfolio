d7024edcd8d8e7207e73d0806731461d6aaf32f2
Hugo Méndez <hello@hugomendez.dev>
2024-03-18 15:08:56 -0500

    feat: update projects

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -11,7 +11,7 @@ const data: Experience[] = [
   { skill: 'Performance', years: 3 },
   { skill: 'TypeScript', years: 2 },
   { skill: 'React', years: 2 },
-  { skill: 'NextJS', years: 1 },
+  { skill: 'NextJS', years: 2 },
 ];
 
 const Skills = () => {
@@ -36,50 +36,3 @@ const Skills = () => {
 };
 
 export default Skills;
-
-const Rings = () => {
-  return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width='530'
-      height='129'
-      className={styles.rings}>
-      <g
-        fill='none'
-        fillRule='evenodd'
-        stroke='#FFF'
-        opacity='.25'>
-        <ellipse
-          cx='265'
-          cy='40'
-          rx='264.5'
-          ry='39.5'
-        />
-        <ellipse
-          cx='265'
-          cy='52'
-          rx='264.5'
-          ry='39.5'
-        />
-        <ellipse
-          cx='265'
-          cy='65'
-          rx='264.5'
-          ry='39.5'
-        />
-        <ellipse
-          cx='265'
-          cy='77'
-          rx='264.5'
-          ry='39.5'
-        />
-        <ellipse
-          cx='265'
-          cy='89'
-          rx='264.5'
-          ry='39.5'
-        />
-      </g>
-    </svg>
-  );
-};
