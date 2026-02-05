d18c2f74b12c91d4df83f573eea38f267164465d
Hugo Méndez <hello@hugomendez.dev>
2026-02-05 16:23:49 -0500

    feat: simplify NotFound component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,6 @@
-import { headers } from 'next/headers';
 import Link from 'next/link';
 
 export default async function NotFound() {
-  const headersList = await headers();
-  const referer = headersList.get('referer') || 'unknown domain';
   return (
     <div
       style={{
@@ -14,7 +11,7 @@ export default async function NotFound() {
         alignContent: 'center',
         gap: '10px',
       }}>
-      <h1>Not Found: {referer}</h1>
+      <h1>Not Found</h1>
       <p>Could not find requested resource.</p>
       <Link
         href='/'
